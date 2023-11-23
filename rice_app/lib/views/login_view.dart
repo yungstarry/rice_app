8415892670ed948429b2e08f56dc86afed32c418
yungstarry <111007711+yungstarry@users.noreply.github.com>
2023-11-23 20:13:18 +0100

    step8

--- rice_app/lib/views/login_view.dart
+++ rice_app/lib/views/login_view.dart
@@ -64,10 +64,16 @@ class _LoginViewState extends State<LoginView> {
                 final userCredential = await FirebaseAuth.instance
                     .signInWithEmailAndPassword(
                         email: email, password: password);
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  notesRoute,
-                  (route) => false,
-                );
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    notesRoute,
+                    (route) => false,
+                  );
+                } else {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                      verifyEmailRoute, (route) => false);
+                }
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
                   // ignore: use_build_context_synchronously
@@ -102,5 +108,3 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 }
-
-
